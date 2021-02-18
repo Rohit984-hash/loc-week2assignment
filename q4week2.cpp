7056c55ec58eb6094d46839b75ddc955e8894a05
Rohit984-hash <71144638+Rohit984-hash@users.noreply.github.com>
2021-02-18 23:23:47 +0530

    Add files via upload

---
+++ q4week2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,large,small;
+	scanf("%d",&n);
+	int arr[n];
+	for(int i=0;i<n;i++){
+	   scanf("%d",&arr[i]);
+	  	}
+	  	large=small=a[0];
+	for(int i=0;i<n;i++){
+	      if(arr[i],small){
+	         small=a[i];
+	         if(a[i]>large);
+	         
+	      }
+	      printf("greater number is %d\n",large);
+	
+	return 0;
+}
